a4e08275494d4f98ea598ab860e8fd3c3e76cd62
spacetree1 <klepikov8561@gmail.com>
2019-09-08 13:37:17 +0000

    sdm710-common: build have overriding commands

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,7 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
-
+BUILD_BROKEN_DUP_RULES := true
 TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
